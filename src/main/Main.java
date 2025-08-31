1a31536249e9adc6491c5bf6277921b041e4f606
alibruno <alissonbruno318@gmail.com>
2025-08-31 00:00:20 -0300

    feat: cadastro de Pets

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import view.VisualizacaoFiles;
+
 public class Main {
     public static void main(String[] args) {
-
+        VisualizacaoFiles visualizacaoFiles = new VisualizacaoFiles();
+        visualizacaoFiles.iniciar();
     }
 }
--- src/view/VisualizacaoFiles.java
+++ src/view/VisualizacaoFiles.java
@@ -1,6 +1,8 @@
 package view;
 
 import dominio.Menu;
+import dominio.Pet;
+import dominio.PetAtributoInvalidoExeception;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,8 +12,9 @@ import java.util.Scanner;
 
 public class VisualizacaoFiles {
     private static final Menu MENU = new Menu();
-    private static final File FILE_MENU_INICIAL = new File("src//arquivos", "menu_inicial.txt");
-    private static final File FILE_FORMULARIO = new File("src//arquivos", "formulario.txt");
+    public static final File FILE_MENU_INICIAL = new File("src//arquivos", "menu_inicial.txt");
+    public static final File FILE_FORMULARIO = new File("src//arquivos", "formulario.txt");
+    private Scanner scanner = new Scanner(System.in);
 
     public void leituraFile(File file) {
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -25,9 +28,7 @@ public class VisualizacaoFiles {
     }
 
     public int escolherOpcao() {
-        Scanner scanner = new Scanner(System.in);
         String opcao;
-        int i = 0;
         while (true) {
             try {
                 opcao = scanner.nextLine();
@@ -35,6 +36,61 @@ public class VisualizacaoFiles {
             } catch (IllegalArgumentException e) {
                 leituraFile(FILE_MENU_INICIAL);
             }
+        }
+    }
+
+    private String iterarSobreAtributosPet(int ordemAtributo, String atributoString) {
+        switch (ordemAtributo) {
+            case 0:
+                return Pet.validarNomeCompleto(atributoString);
+            case 1:
+                return Pet.validarTipo(atributoString);
+            case 2:
+                return Pet.validarSexo(atributoString);
+            case 3:
+                return Pet.validarEndereco(atributoString);
+            case 4:
+                return Pet.validarIdade(atributoString);
+            case 5:
+                return Pet.validarPeso(atributoString);
+            case 6:
+                return Pet.validarRaca(atributoString);
+            default:
+                throw new PetAtributoInvalidoExeception("Ocorreu um erro durante a iteração.");
+        }
+    }
+
+    public void cadastrar() {
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_FORMULARIO))) {
+            String leitura;
+            String[] entrada = new String[7];
+            int iteracao = 0;
+            while ((leitura = br.readLine()) != null) {
+                while (true) {
+                    System.out.println(leitura);
+                    entrada[iteracao] = scanner.nextLine();
+                    try {
+                        entrada[iteracao] = iterarSobreAtributosPet(iteracao, entrada[iteracao]);
+                        iteracao++;
+                        break;
+                    } catch (PetAtributoInvalidoExeception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
+            Pet pet = new Pet(entrada);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void iniciar() {
+        leituraFile(FILE_MENU_INICIAL);
+        int opcao = escolherOpcao();
+        switch (opcao) {
+            case 1:
+                cadastrar();
+                break;
         }
     }
 }
